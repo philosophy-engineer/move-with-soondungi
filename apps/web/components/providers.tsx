8c422df9add7460bd7360ddfb9b195da0847bbf3
2020026635 <dnwjd99@hanyang.ac.kr>
2026-02-18 19:53:20 +0900

    chore(web): tiptap/sonner 의존성과 전역 토스트 설정 추가

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,17 +2,20 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { Toaster } from "sonner"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem
+      defaultTheme="light"
+      forcedTheme="light"
+      enableSystem={false}
       disableTransitionOnChange
       enableColorScheme
     >
       {children}
+      <Toaster richColors position="top-right" closeButton />
     </NextThemesProvider>
   )
 }
